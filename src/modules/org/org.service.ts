3e2ad59e6cb36ae322922836d09f58bd3eba4c69
findreamer <findream@126.com>
2025-11-07 14:58:46 +0800

    refactor(org): 移除创建组织时的冗余实体检查
    这些检查已被注释掉，因为DTO中直接使用外键ID即可，无需先查询实体

--- src/modules/org/org.service.ts
+++ src/modules/org/org.service.ts
@@ -15,30 +15,30 @@ export class OrgService {
   async create(createOrgDto: CreateOrgDto) {
     const { tenant, superior, orgType, leader, name } = createOrgDto;
 
-    // 检查租户是否存在
-    const tenantOrg = await this.orgRepository.findOne({
-      where: { id: tenant, isDeleted: 0 },
-    });
-    if (!tenantOrg) {
-      throw new Error('租户不存在');
-    }
+    // // 检查租户是否存在
+    // const tenantOrg = await this.orgRepository.findOne({
+    //   where: { id: tenant, isDeleted: 0 },
+    // });
+    // if (!tenantOrg) {
+    //   throw new Error('租户不存在');
+    // }
 
-    // 检查上级组织是否存在
-    if (superior) {
-      const superiorOrg = await this.orgRepository.findOne({
-        where: { id: superior, isDeleted: 0 },
-      });
-      if (!superiorOrg) {
-        throw new Error('上级组织不存在');
-      }
-    }
-    // 检查负责人是否存在
-    const leaderOrg = await this.orgRepository.findOne({
-      where: { id: leader, isDeleted: 0 },
-    });
-    if (!leaderOrg) {
-      throw new Error('负责人不存在');
-    }
+    // // 检查上级组织是否存在
+    // if (superior) {
+    //   const superiorOrg = await this.orgRepository.findOne({
+    //     where: { id: superior, isDeleted: 0 },
+    //   });
+    //   if (!superiorOrg) {
+    //     throw new Error('上级组织不存在');
+    //   }
+    // }
+    // // 检查负责人是否存在
+    // const leaderOrg = await this.orgRepository.findOne({
+    //   where: { id: leader, isDeleted: 0 },
+    // });
+    // if (!leaderOrg) {
+    //   throw new Error('负责人不存在');
+    // }
 
     // 如果 DTO 里传的是纯外键 id，直接赋值即可，无需先查询实体
     const org = {
